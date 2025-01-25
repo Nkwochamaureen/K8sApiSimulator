09cadcf98b56825308ef538af405db01bda69f89
Nkwochamaureen <mnkwochae2019@gmail.com>
2025-01-25 02:48:58 -0500

    My first commit

---
+++ main.tf
@@ -0,0 +1,49 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "kubernetes_deployment" "api" {
+  metadata {
+    name = "api-deployment"
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "api"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "api"
+        }
+      }
+      spec {
+        container {
+          image = "api:latest"
+          name  = "api"
+          port {
+            container_port = 5000
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "api" {
+  metadata {
+    name = "api-service"
+  }
+  spec {
+    selector = {
+      app = "api"
+    }
+    port {
+      port        = 80
+      target_port = 5000
+    }
+    type = "NodePort"
+  }
+}
