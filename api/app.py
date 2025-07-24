655ed34f3d75d8fde459e45153fd6de4134cc3da
Nkwochamaureen <mnkwochae2019@gmail.com>
2025-07-24 19:47:06 +0100

    Monitoring

--- api/app.py
+++ api/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
 from datetime import datetime
+from prometheus_flask_exporter import PrometheusMetrics  
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)  
 
 @app.route('/', methods=['GET'])
 def get_time():
@@ -10,7 +12,3 @@ def get_time():
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
-from prometheus_flask_exporter import PrometheusMetrics
-
-app = Flask(__name__)
-metrics = PrometheusMetrics(app)
